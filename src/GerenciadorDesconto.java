e8fcad5350e2aa0453cca073d213a6969cd0c7e7
LucasXavierdeFaria <124639567+LucasXavierdeFaria@users.noreply.github.com>
2024-09-22 22:42:48 -0300

    Exercicio13

---
+++ src/GerenciadorDesconto.java
@@ -0,0 +1,8 @@
+public class GerenciadorDesconto {
+    public void aplicarDesconto(Carrinho carrinho, double percentual) {
+        for (Produto produto : carrinho.getProdutos()) {
+            double precoComDesconto = produto.getPreco() * (1 - percentual / 100);
+            produto.setPreco(precoComDesconto);
+        }
+    }
+}
